492332796901acdffb1ba8914b6cf47a9d874295
sihunh <49368741+sihunh@users.noreply.github.com>
2021-11-10 11:08:45 +0900

    Update App.js

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import { StatusBar } from "expo-status-bar";
-import React from "react";
+import React, {Component} from "react";
 
+import {View, Text} from 'react-native'
 import * as firebase from 'firebase'
 
 const firebaseConfig = {
@@ -21,16 +22,58 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 
 import LandingScreen from "./Components/auth/Landing";
+import RegisterScreen from "./Components/auth/Register";
 
 const Stack = createStackNavigator();
-export default function App() {
-  return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="Landing">
-        <Stack.Screen name="Landing" component={LandingScreen} options={{headerShown: false}}>
 
-        </Stack.Screen>
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
+export class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      loaded: false,
+    }
+  }
+  componentDidMount(){
+  firebase.auth().onAuthStateChanged((user) => {
+    if(!user){
+      this.setState({
+        loggedIn: false,
+        loaded: true,
+      })
+    }else {
+      this.setState({
+        loggedIn: true,
+        loaded: true,
+      })
+    }
+  })
+  }
+  render() {
+    const {loaded, loggedIn} = this.state;
+    if(!loaded){
+      return(
+        <View style={{ flex: 1, justifyContent: 'center'}}>
+          <Text>로딩중</Text>
+        </View>
+      )
+    }
+    if(!loggedIn){
+    return (
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="Landing">
+          <Stack.Screen name="Landing" component={LandingScreen} options={{headerShown: false}}/>
+          <Stack.Screen name="Register" component={RegisterScreen}/>
+        
+        </Stack.Navigator>
+      </NavigationContainer>
+    );
+    }
+
+    return(
+      <View style={{ flex: 1, justifyContent: 'center'}}>
+          <Text>이미 로그인 되어있습니다.</Text>
+        </View>
+    )
+  }
 }
+export default App
