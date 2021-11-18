3e022ea3f9fe36ecad709496f6ad320fd2a96765
sihunh <49368741+sihunh@users.noreply.github.com>
2021-11-18 17:18:31 +0900

    Add files via upload

--- redux/actions/index.js
+++ redux/actions/index.js
@@ -5,13 +5,12 @@ import { USER_STATE_CHANGE } from "../constants/index";
 export function fetchUser(){
     return((dispatch) => {
         firebase.firestore()
-        .collection("user")
+        .collection("users")
         .doc(firebase.auth().currentUser.uid)
         .get()
         .then((snapshot) => {
-            if(snapshot.exists){
-                console.log(snapshot.data())
-                dispatch({type: USER_STATE_CHANGE, currentUser: snapshot.doc()})
+            if(snapshot.exists){ // firestore /users 안에 uid 관한 데이터가 있으면 가져와서 dispatch 시킴
+                dispatch({type: USER_STATE_CHANGE, currentUser: snapshot.data()})
             }
             else{
                 console.log('존재하지 않습니다.')
--- redux/reducers/user.js
+++ redux/reducers/user.js
@@ -2,7 +2,7 @@ const initialState = {
     currentUser: null
 }
 
-export const user = (state = initialState, action)=> {
+export const user = (state = initialState, action)=> { // 유저 상태, 행동 넘김
     return {
         ...state,
         currentUser: action.currentUser
