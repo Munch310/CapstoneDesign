958030d1fff1f25d61feb6fda6b601b8040b816e
backward99 <wjeeho@naver.com>
2021-11-22 18:37:50 +0900

    firebase crud

--- src/components/Words.js
+++ src/components/Words.js
@@ -2,6 +2,25 @@ import React from "react";
 import Card from "@material-ui/core/Card";
 import CardContent from "@material-ui/core/CardContent";
 import Typography from "@material-ui/core/Typography";
+import { withStyles } from "@material-ui/core/styles";
+import Grid from "@material-ui/core/Grid";
+import Button from "@material-ui/core/Button";
+import Fab from "@material-ui/core/Fab";
+import AddIcon from "@material-ui/icons/Add";
+import  Dialog from "@material-ui/core/Dialog";
+import  DialogActions from "@material-ui/core/DialogActions";
+import  DialogContent from "@material-ui/core/DialogContent";
+import  DialogTitle from "@material-ui/core/DialogTitle";
+import  TextField from "@material-ui/core/TextField";
+import { RestoreTwoTone } from "@material-ui/icons";
+
+const styles = theme => ({
+            fab: {
+                position: 'fixed',
+                bottom: '20px',
+                right: '20px'
+            }
+})
 
 const databaseURL = "https://fir-test-eb20a-default-rtdb.firebaseio.com/";
 
@@ -12,6 +31,9 @@ class Words extends React.Component{
         super(props);
         this.state = {
             names: {},
+            dialog: false,
+            name: '',
+            text: ''
         };
     }
     _get(){
@@ -22,13 +44,67 @@ class Words extends React.Component{
             return res.json();
         }).then(names => this.setState({names: names}));
     }
-    shouldComponentUpdate(nextProps, nextState){
-        return nextState.names != this.state.names;
+    _post(name) {
+        return fetch(`${databaseURL}/names.json`,{
+            method: 'POST',
+            body: JSON.stringify(name)
+        }).then(res => {
+            if(res.status != 200) {
+                throw new Error(res.statusText);
+            }
+            return res.json();
+        }).then(data => {
+            let nextState = this.state.names;
+            nextState[data.name] = name;
+            this.setState({names: nextState});
+        });
     }
+    _delete(id) {
+        return fetch(`${databaseURL}/names/${id}.json`, {
+            method: 'DELETE'
+        }).then(res => {
+            if(res.status != 200) {
+                throw new Error(res.statusText);
+            }
+            return res.json();
+        }).then(() => {
+            let nextState = this.state.names;
+            delete nextState[id];
+            this.setState({names: nextState});
+        })
+    }
+   
     componentDidMount() {
         this._get();
     }
+    handleDialogToggle = () => this.setState({
+        dialog: !this.state.dialog
+    })
+    handleValueChange = (e) => {
+        let nextState = {};
+        nextState[e.target.name] = e.target.value;
+        this.setState(nextState);
+    }
+    handleDelete = (id) => {
+        this._delete(id);
+    }
+    handleSubmit = () =>{
+        const name = {
+            name: this.state.name,
+            text: this.state.text
+        }
+        this.handleDialogToggle();
+        if(!name.name) {
+            return;
+        }
+        this._post(name);
+    }
+
+    // shouldComponentUpdate(nextProps, nextState){
+    //     return nextState.names != this.state.names;
+    // }
     render() {
+        const { classes} =this.props;
         return (
             <div>
                 {Object.keys(this.state.names).map( id => {
@@ -40,8 +116,38 @@ class Words extends React.Component{
                                     <Typography color="textSecondary" gutterBottom>
                                         이름 : {name.name}
                                     </Typography>
+                                    <Grid container>
+                                        <Grid item xs={6}>
+                                             <Typography variant="h5" component="h2">
+                                                 {name.text}
+                                             </Typography>
+                                        </Grid>
+                                        <Grid item xs={6}>
+                                             <Button variant="contained" color="primary" onClick={() => this.handleDelete(id)}>
+                                                 삭제
+                                             </Button>
+                                        </Grid>
+                                    </Grid>
                                 </CardContent>
                             </Card>
+                            <Fab color="primary" className={classes.fab} onClick={this.handleDialogToggle}>
+                                <AddIcon/>
+                            </Fab>
+                            <Dialog open={this.state.dialog} onClose={this.handleDialogToggle}>
+                                <DialogTitle>
+                                    이름 추가
+                                </DialogTitle>
+                                <DialogContent>
+                                    <TextField label="작성자" type="text" name="name" value={this.state.name} onChange={this.handleValueChange}/>
+                                    <br/>
+                                    <TextField label="내용" type="text" name="text" value={this.state.text} onChange={this.handleValueChange}/>
+                                    <br/>
+                                </DialogContent>
+                                <DialogActions>
+                                    <Button variant="contained" color="primary" onClick={this.handleSubmit}>추가</Button>
+                                    <Button variant="outlined" color="primary" onClick={this.handleDialogToggle}>닫기</Button>
+                                </DialogActions>
+                            </Dialog>
                         </div>
                     );
                 })}
@@ -51,4 +157,4 @@ class Words extends React.Component{
     }
 }
 
-export default Words;
+export default withStyles(styles)(Words);
